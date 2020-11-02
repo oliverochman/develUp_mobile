c47aca8de9a60dc841fb0dca0f0bd966d4b8b389
FacundoOs <osores.facundo@gmail.com>
2020-11-02 13:18:04 +0100

    add view for assignments. Create AssignmentCard component return the assignment card. Formatting

---
+++ src/components/AssignmentCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const AssignmentCard = ({ assignment }) => {
+  return <View></View>;
+};
+
+export default AssignmentCard;
--- src/components/AssignmentsIndex.js
+++ src/components/AssignmentsIndex.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import AssignmentCard from "./AssignmentCard";
 
 const AssignmentsIndex = () => {
-	const [assignments, setAssignments ] = useState ([])
+  const [assignments, setAssignments] = useState([]);
 
-	useEffect(() => {
-		const getAssignmentsIndex = async () => {
-			const response = await Assignments.index()
-			setAssignments(response)
-		}
-		getAssignmentsIndex();
-	}, [])
+  useEffect(() => {
+    const getAssignmentsIndex = async () => {
+      const response = await Assignments.index();
+      setAssignments(response);
+    };
+    getAssignmentsIndex();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text>Welcome to DevelUp</Text>
+      <FlatList
+        data={assignments}
+        keyExtractor={(assignment) => assignment.id.toString()}
+        renderItem={({ item }) => {
+          return <AssignmentCard assignment={item} />;
+        }}
+      />
     </View>
   );
 };
