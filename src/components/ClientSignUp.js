790b2f4b274945c7f84b9087367fdc815b883f17
sara <saralundkvist86@gmail.com>
2020-11-03 17:16:38 +0100

    creates form shell for sign up

--- src/components/ClientSignUp.js
+++ src/components/ClientSignUp.js
@@ -1,14 +1,51 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Container,
+  Button,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Text,
+} from "native-base";
 
 const ClientSignUp = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <Container>
+      <Content>
+        <Form>
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input />
+          </Item>
+
+          <Item floatingLabel last>
+            <Label>Password</Label>
+            <Input secureTextEntry />
+          </Item>
+
+          <Item floatingLabel last>
+            <Label>Password Confirmation</Label>
+            <Input secureTextEntry />
+          </Item>
+
+          <Item floatingLabel last>
+            <Label>Company Name</Label>
+            <Input />
+          </Item>
+
+          <Item floatingLabel last>
+            <Label>Company URL</Label>
+            <Input />
+          </Item>
+        </Form>
+      </Content>
+      <Button block>
+        <Text>Submit</Text>
+      </Button>
+    </Container>
   );
 };
 
 export default ClientSignUp;
-
-const styles = StyleSheet.create({});
