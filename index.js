ccebf005759720bf1808359c50e3a0fac5731fb3
FacundoOs <osores.facundo@gmail.com>
2020-11-03 14:35:23 +0100

    remove v1 from path

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { registerRootComponent } from "expo";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://develup-2020.herokuapp.com/api/v1";
+axios.defaults.baseURL = "https://develup-2020.herokuapp.com/api";
 
 registerRootComponent(App);
