73e144d53e1b8116c15ac2fd803000d3ba4f4ff3
emiliano-ma <emilianomainero@gmail.com>
2020-11-04 08:23:42 +0100

    Adding input fields to the create assignment form

--- src/components/AssignmentForm.js
+++ src/components/AssignmentForm.js
@@ -1,12 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native'
+import {
+  Container,
+  Button,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+} from "native-base";
 
 
 const AssignmentForm = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [timeframe, setTimeframe] = useState("");
+  const [budget, setBudget] = useState("");
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <Container>
+    <Content>
+      <Form>
+        <Item floatingLabel>
+          <Label>Title</Label>
+          <Input onChangeText={(text) => setTitle(text)} />
+        </Item>
+
+        <Item floatingLabel last>
+          <Label>Description</Label>
+          <Input onChangeText={(text) => setDescription(text)}  />
+        </Item>
+
+        <Item floatingLabel last>
+          <Label>Timeframe</Label>
+          <Input
+            onChangeText={(text) => setTimeframe(text)}
+            
+          />
+        </Item>
+
+        {/* <Item floatingLabel last>
+          <Label>SKILLS CHECKBOX</Label>
+          <Input
+            onChangeText={(text) => setPasswordConfirmation(text)}
+            
+          />
+        </Item> */}
+
+        <Item floatingLabel last>
+          <Label>Budget</Label>
+          <Input onChangeText={(text) => setBudget(text)} />
+        </Item>
+
+      </Form>
+
+    </Content>
+    <Button block onPress={() => signUpHandler()}>
+      <Text>Submit</Text>
+    </Button>
+  </Container>
   )
 }
 
